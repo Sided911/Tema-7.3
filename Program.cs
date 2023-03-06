737617d5aebb8b5b48bc955f69d29b814935d4eb
Sided911 <51265600+Sided911@users.noreply.github.com>
2023-03-06 08:58:20 +0200

    1 задание
    сделал 1 задание

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1 задание 
+            Random random = new Random();
+            for (int i = 1; i<= 10; i++)
+            {
+                int x = random.Next(-40, 40);
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
         }
     }
 }
